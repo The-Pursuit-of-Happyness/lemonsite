f7e517d2630dab99ff7e00e249521dd136f861e5
never-give-up <1025582613@qq.com>
2019-04-10 19:28:05 +0800

    修改mongodb地址

--- mock/userDB.js
+++ mock/userDB.js
@@ -1,13 +1,12 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb://66.42.42.120:25434';
 
-function handleData(callback){
-    MongoClient.connect(url,(err,database) =>{
-        const myAwesomeDB = database.db('admin')
-        let collection = myAwesomeDB.collection('user');
-        callback(collection)
-    });
+function handleData(callback) {
+  MongoClient.connect(url, (err, database) => {
+    const myAwesomeDB = database.db('admin')
+    let collection = myAwesomeDB.collection('user');
+    callback(collection)
+  });
 }
 
 module.exports = handleData;
-
